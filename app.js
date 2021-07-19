0124895b6227a0fe3e66fe1156c861e3c532ae66
pandeymadhuri <madhuripandeyb1993@gmail.com>
2021-07-20 03:27:17 +0530

    added update and delete route

--- app.js
+++ app.js
@@ -1,12 +1,72 @@
  const express = require ('express');
  const app = express();
 
- const mongoose = require('./database/mongoose');
+ const TaskList = require ('./database/models/TaskList');
+ const Task = require ('./database/models/Task');
 
-/* app.listen(3000,function(){
-   console.log("server started on port 3000");  
- });*/
+ //direct way to do by adding 3rd party library app.use(cors());
+ //example of middleware
+ app.use(function(req,res,next){
+   res.setHeader('Access-Control-Allow-Origin','*');
+   res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE');
+   res.setHeader('Access-Control-Allow-Headers','origin,X-Requested with, Content-Type,Accepted');
+   next();
+ });
+ app.use(express.json());
+
+ const mongoose = require('./database/mongoose');
+ app.get('/tasklists',(req,res)=>{
+   TaskList.find({})
+   .then((lists)=>{res.status(200).send(lists)
+ })
+   .catch((error)=>{
+     console.log(error);
+     res.status(500);
+   });
+ });
+ app.get(
+   'tasklists/:tasklistId',(req,res)=>{
+     let tasklistId =req.params.tasklistId;
+    TaskList.find({_id:tasklistId})
+    .then((taskList)=>{
+    res.status(200).send(taskList)})
+    .catch((error)=> {console.log(error)});
+   }
+ );
+   
+ app.post('/tasklists',(req,res)=>{
+   console.log(req.body);
+
+   let taskListObj = {'title':req.body.title};
+   TaskList(taskListObj).save()
+   .then((taskList)=>{res.status(201).send(taskList)
+   })
+     .catch((error)=>{
+       console.log(error);
+       res.status(500);
+     });
+   });
+   app.put('tasklists/:tasklistId',(req,res)=>{
+      TaskList.findOneAndUpdate({_id:req.params.tasklistId},{$set : req.body})
+      .then((taskList)=>{
+        res.status(200).send(taskList)})
+        .catch((error)=> {console.log(error)});
+   });
+
+   app.patch('tasklists/:tasklistId',(req,res)=>{
+    TaskList.findOneAndUpdate({_id:req.params.tasklistId},{$set : req.body})
+    .then((taskList)=>{
+      res.status(200).send(taskList)})
+      .catch((error)=> {console.log(error)});
+ });
+
+ app.delete('tasklists/:tasklistId',(req,res)=>{
+  TaskList.findByIdAndDelete({req.params.tasklistId})
+  .then((taskList)=>{
+    res.status(201).send(taskList)})
+    .catch((error)=> {console.log(error)});
+});
 
  app.listen(3000,()=> {
-    console.log("server started on port 3000 GO");  
+    console.log("server started on port 3000 GO ");  
   });
