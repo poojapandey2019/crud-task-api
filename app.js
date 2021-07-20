6c77dfdf4d458a866a75a88e97bded512b694509
pandeymadhuri <madhuripandeyb1993@gmail.com>
2021-07-20 18:08:48 +0530

    tasklist and tasks created

--- app.js
+++ app.js
@@ -34,18 +34,6 @@
    }
  );
    
- app.post('/tasklists',(req,res)=>{
-   console.log(req.body);
-
-   let taskListObj = {'title':req.body.title};
-   TaskList(taskListObj).save()
-   .then((taskList)=>{res.status(201).send(taskList)
-   })
-     .catch((error)=>{
-       console.log(error);
-       res.status(500);
-     });
-   });
    app.put('tasklists/:tasklistId',(req,res)=>{
       TaskList.findOneAndUpdate({_id:req.params.tasklistId},{$set : req.body})
       .then((taskList)=>{
@@ -60,13 +48,39 @@
       .catch((error)=> {console.log(error)});
  });
 
- app.delete('tasklists/:tasklistId',(req,res)=>{
-  TaskList.findByIdAndDelete({req.params.tasklistId})
+ app.delete('tasklists/:tasklistId',(req,res)=>{ 
+  TaskList.findByIdAndDelete(req.params.tasklistId)
   .then((taskList)=>{
     res.status(201).send(taskList)})
     .catch((error)=> {console.log(error)});
 });
 
+app.get('tasklists/:tasklistId/tasks',(req,res)=>{
+ Task.find({_taskListId:req.params.tasklistId})
+ .then((tasks)=>{
+  res.status(200).send(tasks)})
+  .catch((error)=> {console.log(error)});
+});
+
+app.post('/tasklists/:tasklistId/tasks',(req,res)=>{
+  console.log(req.body);
+
+  let taskObj = {'title':req.body.title,'_taskListId': req.params.tasklistId};
+  Task(taskObj).save()
+  .then((task)=>{res.status(201).send(task)
+  })
+    .catch((error)=>{
+      console.log(error);
+      res.status(500);
+    });
+  });
+  app.get('tasklists/:tasklistId/tasks/taskId',(req,res)=>{
+    Task.find({_taskListId:req.params.tasklistId, _id:req.params.taskId})
+    .then((task)=>{
+     res.status(200).send(task)})
+     .catch((error)=> {console.log(error)});
+   });
+   
  app.listen(3000,()=> {
     console.log("server started on port 3000 GO ");  
   });
